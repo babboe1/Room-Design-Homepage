b9d3d0cd589d36214e6811bc68131f5cf082524d
babboe1 <ayodelaustinab@gmail.com>
2022-06-06 09:34:08 +0100

    feat: create constant DOM variable

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,2 +1,4 @@
 const BACKDROP = document.getElementById('backdrop');
 const MENU_BTN = document.querySelector('.MenuBtn');
+const [ARROW_MOBILE, ARROW_DESKTOP] = document.querySelectorAll('.NavArrow');
+const ARROW_MOBILE_LEFT = ARROW_MOBILE.firstElementC
