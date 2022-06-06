6a089b07f389c75070eb0d7bac29c034ebac27eb
babboe1 <ayodelaustinab@gmail.com>
2022-06-06 09:34:57 +0100

    feat: create constant DOM variable

--- assets/script/app.js
+++ assets/script/app.js
@@ -4,4 +4,6 @@ const [ARROW_MOBILE, ARROW_DESKTOP] = document.querySelectorAll('.NavArrow');
 const ARROW_MOBILE_LEFT = ARROW_MOBILE.firstElementChild;
 const ARROW_MOBILE_RIGHT = ARROW_MOBILE.lastElementChild;
 const ARROW_DESKTOP_LEFT = ARROW_DESKTOP.firstElementChild;
+const ARROW_DESKTOP_RIGHT = ARROW_DESKTOP.lastElementChild;
+const MENU_MODAL = document.getElementById('menu-modal');
 
